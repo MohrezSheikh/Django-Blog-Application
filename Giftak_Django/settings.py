e9e96a049113dce5160eff87fa0c616fc786d265
Mohamad Reza Sheikh <68335653+mmdation@users.noreply.github.com>
2021-09-26 08:03:22 +0330

    Update settings.py

--- Giftak_Django/settings.py
+++ Giftak_Django/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dd8++7z!-_k4f&r1jn9uvtmc)!(^0w8glphkmta@e4xic&f1@q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
